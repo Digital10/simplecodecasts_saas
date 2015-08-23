fdda338c0e545e015cbbbcda8502601984b84b17
Sean Mystyk <seanmystyk@outlook.com>
2015-08-23 20:40:29 +0000

    Mailer update

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,11 @@
 class ContactMailer < ActionMailer::Base
-   default to: 'seanmystyk@outlook.com'
+   default to: 'mystyk@outlook.com'
    
    def contact_email(name, email, body)
        @name = name
        @email = email
        @body = body
        
-       mail(fromL email, subject: 'Contact Form Message')
+       mail(from: email, subject: 'Contact Form Message')
    end
 end 
