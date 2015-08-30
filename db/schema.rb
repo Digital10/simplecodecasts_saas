05af62cd32eb5521c0cd3d55dacbe2c142af3447
Sean Mystyk <seanmystyk@outlook.com>
2015-08-30 17:37:27 +0000

    Added profile model and table

--- db/schema.rb
+++ db/schema.rb
@@ -11,36 +11,48 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150824194815) do
+ActiveRecord::Schema.define(version: 20150830160953) do
 
-  create_table "contacts", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.string   "email",      limit: 255
+  create_table "contacts", force: true do |t|
+    t.string   "name"
+    t.string   "email"
     t.text     "comments"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "plans", force: :cascade do |t|
-    t.string  "name",  limit: 255
+  create_table "plans", force: true do |t|
+    t.string  "name"
     t.decimal "price"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  limit: 255, default: "", null: false
-    t.string   "encrypted_password",     limit: 255, default: "", null: false
-    t.string   "reset_password_token",   limit: 255
+  create_table "profiles", force: true do |t|
+    t.integer  "user_id"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "job_title"
+    t.string   "phone_number"
+    t.string   "contact_email"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "email",                              default: "", null: false
+    t.string   "encrypted_password",                 default: "", null: false
+    t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.integer  "sign_in_count",                      default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip",     limit: 255
-    t.string   "last_sign_in_ip",        limit: 255
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "plan_id"
-    t.string   "stripe_customer_token"
+    t.string   "stripe_customer_token",  limit: nil
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
